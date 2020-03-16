89c1f4611a47c41239e9ba24f31a4016755ef4a5
Robin Schroer <sulami@peerwire.org>
2020-03-16 11:30:52 +0100

    Add licensing bits

--- ox-org-hugo-sidenotes.el
+++ ox-org-hugo-sidenotes.el
@@ -1,5 +1,28 @@
 ;;; ox-org-hugo-sidenotes.el --- Org Tufte backend for Org Export Engine
 
+;; Copyright (C) 2020 Robin Schroer
+
+;; Author: Robin Schroer <sulami@peerwire.org>
+;; Version: 0.1-SNAPSHOT
+;; Keywords: outlines, hypermedia, wp
+;; Homepage: https://github.com/sulami/ox-org-hugo-sidenotes.el
+;; Package-Requires: ((org "8.3"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; This package adapts outputs modified org-mode to work with Hugo
